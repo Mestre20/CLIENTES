c2987197d573dda9cebf5043ad4e95ef2ad9da1f
Mixtel <Mixtel@DESKTOP-OHEEO9B>
2024-06-07 12:21:17 -0300

    Adicionar arquivos de projeto.

---
+++ CLIENTES/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ClienteManager
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button5;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.comboBoxTecnico = new System.Windows.Forms.ComboBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nome.Location = new System.Drawing.Point(196, 21);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(354, 38);
+            this.txt_nome.TabIndex = 0;
+            this.txt_nome.TextChanged += new System.EventHandler(this.txt_nome_TextChanged);
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_numero.Location = new System.Drawing.Point(196, 72);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(354, 38);
+            this.txt_numero.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(196, 192);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(354, 29);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(228, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Adicionar Cliente";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(31, 303);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(659, 184);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(96, 493);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(364, 29);
+            this.dateTimePicker2.TabIndex = 5;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(466, 491);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 33);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Baixar Lista";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(31, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "NOME";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "NUMERO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "DATA DA ATIVAÇÃO";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::CLIENTES.Properties.Resources.image__1_;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(151, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 48);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Image = global::CLIENTES.Properties.Resources.image__1_;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(151, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 48);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.ErrorImage = null;
+            this.pictureBox3.Image = global::CLIENTES.Properties.Resources.image__1_;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(151, 188);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 48);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.ErrorImage = null;
+            this.pictureBox4.Image = global::CLIENTES.Properties.Resources.Add_User_80_icon_icons_com_57380;
+            this.pictureBox4.InitialImage = null;
+            this.pictureBox4.Location = new System.Drawing.Point(435, 227);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(69, 70);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(31, 132);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 37);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "TECNICO";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(196, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 38);
+            this.textBox1.TabIndex = 15;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.ErrorImage = null;
+            this.pictureBox5.Image = global::CLIENTES.Properties.Resources.image__1_;
+            this.pictureBox5.InitialImage = null;
+            this.pictureBox5.Location = new System.Drawing.Point(151, 126);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(42, 48);
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // comboBoxTecnico
+            // 
+            this.comboBoxTecnico.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTecnico.FormattingEnabled = true;
+            this.comboBoxTecnico.Items.AddRange(new object[] {
+            "Guilherme",
+            "Rafael",
+            "Wanderson",
+            "Joao Murilo"});
+            this.comboBoxTecnico.Location = new System.Drawing.Point(196, 125);
+            this.comboBoxTecnico.Name = "comboBoxTecnico";
+            this.comboBoxTecnico.Size = new System.Drawing.Size(354, 39);
+            this.comboBoxTecnico.TabIndex = 17;
+            this.comboBoxTecnico.SelectedIndexChanged += new System.EventHandler(this.comboBoxTecnico_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(567, 264);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 33);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Limpar Data";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::CLIENTES.Properties.Resources.abstract_background_with_low_poly_design__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(748, 572);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.comboBoxTecnico);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.txt_nome);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gerenciador de Clientes Rafael";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.ComboBox comboBoxTecnico;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button7;
+    }
+}
---
+++ CLIENTES/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using OfficeOpenXml;
+
+namespace ClienteManager
+{
+    public partial class Form1 : Form
+    {
+        private List<Cliente> clientes = new List<Cliente>();
+        private string dataFilePath = "clientes.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadClientesFromFile();
+
+            // Definir o contexto de licença do EPPlus
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // Adicionar o evento TextChanged para txt_nome
+            txt_nome.TextChanged += txt_nome_TextChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtualizarListaClientes();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string nome = txt_nome.Text;
+            string numero = txt_numero.Text;
+            DateTime dataAtivacao = dateTimePicker1.Value;
+            string nomeTecnico = comboBoxTecnico.SelectedItem.ToString();
+
+            Cliente cliente = new Cliente
+            {
+                Nome = nome,
+                Numero = numero,
+                DataAtivacao = dataAtivacao,
+                NomeTecnico = nomeTecnico
+            };
+
+            clientes.Add(cliente);
+            AtualizarListaClientes();
+            SaveClientesToFile();
+
+            txt_nome.Clear();
+            txt_numero.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            comboBoxTecnico.SelectedIndex = -1;
+        }
+
+        private void AtualizarListaClientes()
+        {
+            listBox1.Items.Clear();
+            foreach (var cliente in clientes)
+            {
+                listBox1.Items.Add(cliente.ToString());
+            }
+        }
+
+        private void AtualizarListaClientesPorData()
+        {
+            listBox1.Items.Clear();
+            DateTime dataSelecionada = dateTimePicker2.Value.Date;
+            var clientesFiltrados = clientes.Where(c => c.DataAtivacao.Date == dataSelecionada).ToList();
+
+            foreach (var cliente in clientesFiltrados)
+            {
+                listBox1.Items.Add(cliente.ToString());
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            AtualizarListaClientesPorData();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DateTime dataSelecionada = dateTimePicker2.Value.Date;
+            var clientesFiltrados = clientes.Where(c => c.DataAtivacao.Date == dataSelecionada).ToList();
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                saveFileDialog.Title = "Salvar Lista de Clientes";
+                saveFileDialog.FileName = "clientes.xlsx";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo file = new FileInfo(saveFileDialog.FileName);
+                    using (ExcelPackage package = new ExcelPackage(file))
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Clientes");
+
+                        // Escrever cabeçalho
+                        worksheet.Cells[1, 1].Value = "Nome";
+                        worksheet.Cells[1, 2].Value = "Telefone";
+                        worksheet.Cells[1, 3].Value = "Técnico";
+
+                        // Escrever dados dos clientes
+                        int row = 2;
+                        foreach (var cliente in clientesFiltrados)
+                        {
+                            worksheet.Cells[row, 1].Value = cliente.Nome;
+                            worksheet.Cells[row, 2].Value = cliente.Numero;
+                            worksheet.Cells[row, 3].Value = cliente.NomeTecnico;
+                            row++;
+                        }
+
+                        package.Save();
+                    }
+
+                    MessageBox.Show("Lista de clientes baixada com sucesso!");
+                }
+            }
+        }
+
+        private void SaveClientesToFile()
+        {
+            string json = JsonConvert.SerializeObject(clientes, Formatting.Indented);
+            File.WriteAllText(dataFilePath, json);
+        }
+
+        private void LoadClientesFromFile()
+        {
+            if (File.Exists(dataFilePath))
+            {
+                string json = File.ReadAllText(dataFilePath);
+                clientes = JsonConvert.DeserializeObject<List<Cliente>>(json) ?? new List<Cliente>();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveClientesToFile();
+        }
+
+        private void txt_nome_TextChanged(object sender, EventArgs e)
+        {
+            txt_nome.Text = txt_nome.Text.ToUpper();
+            txt_nome.SelectionStart = txt_nome.Text.Length;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxTecnico_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            LimparClientesPorData();
+        }
+
+        private void LimparClientesPorData()
+        {
+            DateTime dataSelecionada = dateTimePicker2.Value.Date;
+            var clientesFiltrados = clientes.Where(c => c.DataAtivacao.Date == dataSelecionada).ToList();
+
+            foreach (var cliente in clientesFiltrados)
+            {
+                clientes.Remove(cliente);
+            }
+
+            AtualizarListaClientes();
+            SaveClientesToFile();
+        }
+
+    }
+
+
+}
+
+
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public DateTime DataAtivacao { get; set; }
+        public string NomeTecnico { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Numero} - {DataAtivacao.ToShortDateString()} - {NomeTecnico}";
+        }
+    }
+
